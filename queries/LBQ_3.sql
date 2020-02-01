1c268900dcc4a35b38f837a55c51746213f9dd8b
David-NMN-Melllow <TheMellows@MacBook-Pro-4.local>
2020-02-01 13:34:36 -0600

    Q3 02_01

--- queries/LBQ_3.sql
+++ queries/LBQ_3.sql
@@ -4,17 +4,18 @@
 	Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
 	Sort this list in descending order by the total salary earned. 
 	Which Vanderbilt player earned the most money in the majors?
+	
     SOURCES ::
-
+	people, college playing, salaries tables
 
     DIMENSIONS ::
-        * ...
+    namefirst, namelast, givenname,
 
     FACTS ::
-        * ...
+    Total salary
 
     FILTERS ::
-        * ...
+    MAX, LIMIT
 
     DESCRIPTION ::
         ...
