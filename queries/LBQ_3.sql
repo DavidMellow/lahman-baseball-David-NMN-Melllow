39adb1e48ae96a66d785f01adecd9fb4ddedf228
David-NMN-Melllow <TheMellows@MacBook-Pro-4.local>
2020-02-04 19:26:16 -0600

    real final

--- queries/LBQ_3.sql
+++ queries/LBQ_3.sql
@@ -37,13 +37,13 @@ FROM people AS p
 	ON colp.schoolid = sch.schoolid
 WHERE sch.schoolname ILIKE '%Vanderbilt%';
 /*SELECT *
-FROM vandy  -- check table read out*/
+FROM vandy  -- check temp table read out*/
 /*SELECT COUNT (full_name)
 FROM vandy --24 */
 /*SELECT COUNT (DISTINCT playerid)
 FROM collegeplaying
 WHERE collegeplaying.schoolid ILIKE '%Vand%'; --24 checks */
-/*SELECT 
+SELECT 
 	DISTINCT full_name,
 	SUM(salary) AS total_salary_earn
 FROM salaries
@@ -51,13 +51,13 @@ JOIN vandy
 ON salaries.playerid = vandy.playerid
 GROUP BY full_name
 ORDER BY total_salary_earn DESC
-LIMIT 1;*/
-SELECT
+LIMIT 1;
+/*SELECT
 	DISTINCT full_name,
 	SUM(salary) AS total_salary_earn
 FROM vandy
 FULL OUTER JOIN salaries
 ON vandy.playerid = salaries.playerid
 GROUP BY full_name
-HAVING SUM(salary) IS NULL;--trying to see why 24 Vandy players but only 15 with salaries
+HAVING SUM(salary) IS NULL;--trying to see why 24 Vandy players but only 15 with salaries */
 
