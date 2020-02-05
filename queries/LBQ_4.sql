55f3cf0616b1bc0b52e78a0b9ca1570db202dea3
David-NMN-Melllow <TheMellows@MacBook-Pro-4.local>
2020-02-04 20:02:30 -0600

    Q4 answer

--- queries/LBQ_4.sql
+++ queries/LBQ_4.sql
@@ -5,25 +5,44 @@
 		those with position "SS", "1B", "2B", and "3B" as "Infield", 
 		and those with position "P" or "C" as "Battery". 
 	Determine the number of putouts made by each of these three groups in 2016.
+	
     SOURCES ::
-
+	fielding table
 
     DIMENSIONS ::
-        * ...
+     position
 
     FACTS ::
-        * ...
+     put outs
 
     FILTERS ::
-        * ...
+    SUM, CASE, WHERE, GROUP
 
     DESCRIPTION ::
-        ...
+     Summed put outs and agregated positions per the question. Limited to put outs in 2016
 
     ANSWER ::
-        ...
-
+       Battery 41424 put outs in 2016
+	   Outfield 29560 put outs in 2016
+	   Infield 58934 put outs in 2016
+	   (129918 total put outs)
 */
 
-SELECT *
-FROM ...;
+/*SELECT DISTINCT POS
+FROM fielding; --check position abbreviations in fielding.pos */
+
+SELECT 
+	SUM (po) AS po_2016,
+	CASE 
+		WHEN pos = 'OF' THEN 'outfield'
+		WHEN pos = '1B' OR pos = '2B' OR pos = 'SS' or pos = '3B' THEN 'infield'
+		ELSE 'battery' END AS position
+FROM fielding AS f
+WHERE yearid = '2016'
+GROUP BY position;
+
+/*SELECT yearid, SUM(po)
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY yearid; --129918  matches sum of three position groups*/
+
